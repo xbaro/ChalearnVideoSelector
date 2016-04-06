380d49c2c95c5ef53575686c4b7c210f61bc2552
Xavier Baró <xbaro@uoc.edu>
2016-04-06 20:23:00 +0200

    Solved some issues Added stylesheets Added direct links to the pages

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,19 @@ var DB = require('../db');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });    
+    res.render('index', { title: 'Chalearn Video Selector' });    
 });
 
 router.get('/stats', function (req, res) {
     if (!req.isAuthenticated()) {
         res.redirect('/signin');
     } else {
-        var knex = DB.DB.knex;       
+        var knex = DB.DB.knex;
 
-        //select("labeler, count(*) as total, SUM(labeled = 1) AS TL, SUM(labeled = 0) AS TNL, SUM(label = '1') AS T1, SUM(label = '2') AS T2, SUM(label = '3') AS T3")
-        knex.select(knex.raw("labeler, count(*) as total, SUM(labeled = 1) AS TL, SUM(labeled = 0) AS TNL, SUM(label = '1') AS T1, SUM(label = '2') AS T2, SUM(label = '3') AS T3")).from('tblvideos')        
-            .groupBy('labeler').orderBy('TL', 'desc').then(function (userData) {
+        knex.select(knex.raw("labeler, count(*) as total, SUM(labeled = 1) AS TL, SUM(labeled = 0) AS TNL,coalesce(SUM(label ='1') , 0) AS TAccepted, coalesce(SUM(label ='2') , 0) AS TRejected, coalesce(SUM(label ='3') , 0) AS TMicroCuts")).from('tblvideos')        
+            .whereNot('labeler', null).groupBy('labeler').orderBy('TL', 'desc').then(function (userData) {
             
-            knex.select(knex.raw("count(*) as total, SUM(labeled = 1) AS TL, SUM(labeled = 0) AS TNL, SUM(label = '1') AS T1, SUM(label = '2') AS T2, SUM(label = '3') AS T3")).from('tblvideos')
+            knex.select(knex.raw("count(*) as total, SUM(labeled = 1) AS TL, SUM(labeled = 0) AS TNL, coalesce(SUM(label ='1') , 0) AS TAccepted, coalesce(SUM(label ='2') , 0) AS TRejected, coalesce(SUM(label ='3') , 0) AS TMicroCuts")).from('tblvideos')
             .then(function (videoData) {
                 res.render('stats', { title: 'Statistics', username: req.user.attributes.username, videoData: videoData[0], userData: userData });                        
             })
