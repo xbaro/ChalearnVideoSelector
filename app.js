377ffab8b60c377a8243d822d571f5a347e77502
Xavier Baró <xbaro@uoc.edu>
2016-04-16 22:07:55 +0200

    Added favicon.ico Changed stylus parameters to try to improve css generation

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(require('stylus').middleware(path.join(__dirname, 'public'), {force: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* BEGIN: Configure Passport for user authentication */
